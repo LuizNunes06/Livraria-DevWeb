6042fcc51175786cc008ec5b65596be1acdaf5ae
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-08-02 08:45:55 -0300

    Criando relacionamentos de livro com coAutor

--- core/models/livro.py
+++ core/models/livro.py
@@ -10,9 +10,10 @@ class Livro(models.Model):
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0, blank=True, null=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, blank=True, null=True)
-    autores = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name="livros", blank=True, null=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros", blank=True, null=True)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", blank=True, null=True)
+    autores = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name="livrosAutor", blank=True, null=True) #livro com 1 autor
+    coautor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name="livrosCoautor", blank=True, null=True) #livro com 1 coautor
 
     def __str__(self):
         return f"({self.id}) {self.titulo} ({self.quantidade})"
