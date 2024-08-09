cf9098c924964b4588e543b0343919964e54c492
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-08-09 08:35:17 -0300

    Dois serializadores para Livro

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -7,4 +7,10 @@ class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = "__all__"
-        depth = 1
+
+
+class LivroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1 # Permite a apresentação dos dados relacionados, mostras nos valores no lugar do id.
