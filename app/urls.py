bbfa783e3ca0cd8125cbab17c0aa984dc723e712
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-09-27 09:07:38 -0300

    Criação de um endpoint para atualizar compras

--- app/urls.py
+++ app/urls.py
@@ -11,14 +11,7 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import (
-    AutorViewset,
-    CategoriaViewSet,
-    EditoraViewSet,
-    LivroViewset,
-    UserViewSet,
-    CompraViewSet
-)
+from core.views import AutorViewset, CategoriaViewSet, EditoraViewSet, LivroViewset, UserViewSet, CompraViewSet
 from uploader.router import router as uploader_router
 
 router = DefaultRouter()
