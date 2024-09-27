d40959581951547298a70f8db216f692a611e090
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-09-27 08:44:18 -0300

    Criação de um endpoint para criar novas compras

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -3,4 +3,4 @@ from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .livro import LivroDetailSerializer, LivroListSerializer, LivroSerializer
 from .user import UserSerializer
-from .compra import CompraSerializer
+from .compra import CompraSerializer, CriarEditarCompraSerializer, ItensCompraSerializer
